a1d52dbc93e755884d1ad0775a71fcd287afc220
JKobayashi98 <james.m.kobayashi@gmail.com>
2021-01-31 13:17:36 -0300

    correcting the serializer

--- creditCardChallenge/serializer.py
+++ creditCardChallenge/serializer.py
@@ -3,5 +3,5 @@ from creditCardChallenge.models import CreditCard
 
 class CreditCardSerializer(serializers.ModelSerializer):
     class Meta:
-        model: CreditCard
+        model = CreditCard
         fields = ['id','exp_date','holder','number','cvv']
