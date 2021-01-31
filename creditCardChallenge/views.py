60d8824ea27e53c9939575de80f15843e9a72874
JKobayashi98 <james.m.kobayashi@gmail.com>
2021-01-31 13:46:22 -0300

    Creating the CreditCard GET method

--- creditCardChallenge/views.py
+++ creditCardChallenge/views.py
@@ -11,5 +11,15 @@ class creditCardsViewSet(APIview):
 
     def get(self,request):
         credit_card_list = CreditCard.objects.all()
+        
+        # Verifying if ID is in the parameters passed in GET REQUEST
+        credit_card_id = request.query_params.get('id',None)
+        
+        # if a parameter is passed filter results by parameter
+        if credit_card_id is not None:
+            credit_card_list = credit_card_list.filter(id=credit_card_id)
+        
+        serializer_result = CreditCardSerializer
+        return serializer_result
 
-        credit_card_id = 
+
