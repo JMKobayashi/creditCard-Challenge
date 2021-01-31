912a43e0f068728f6800c68af09a3b084490a7e9
JKobayashi98 <james.m.kobayashi@gmail.com>
2021-01-31 10:41:06 -0300

    correcting the app name and removing max_length

--- creditCardChallenge/models.py
+++ creditCardChallenge/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class CreditCard(models.Model):
     exp_date = models.DateField()
     holder = models.CharField(max_length=30)
-    number = models.IntegerField(max_length=16)
-    cvv = models.IntegerField(max_length=4)
+    number = models.IntegerField()
+    cvv = models.IntegerField()
 
     def __str__(self):
         return self.holder
