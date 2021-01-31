84e5ec0303abc06fc0f07df5a25fc81e5eb5719f
JKobayashi98 <james.m.kobayashi@gmail.com>
2021-01-31 11:03:34 -0300

    creating admin file for the CreditCard model

--- creditCardChallenge/admin.py
+++ creditCardChallenge/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from creditCardChallenge.models import CreditCard
 
-# Register your models here.
+class CreditCards(admin.ModelAdmin):
+    list_display = ('id','exp_date','holder','number','cvv')
+    list_display_links = ('id','number')
+    search_fields = ('id','number')
+    list_per_page = 10
+
+admin.site.register(CreditCard,CreditCards)
