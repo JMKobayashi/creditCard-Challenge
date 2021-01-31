d9dfadc695fe108aa6b29bb29c4e4ad82cb19800
JKobayashi98 <james.m.kobayashi@gmail.com>
2021-01-31 15:13:14 -0300

    Don´t accept null on brand in models

--- creditCardChallenge/models.py
+++ creditCardChallenge/models.py
@@ -4,7 +4,7 @@ class CreditCard(models.Model):
     exp_date = models.CharField(max_length=10,blank=False)
     holder = models.CharField(max_length=30,blank=False)
     number = models.CharField(max_length=16,blank=False)
-    cvv = models.CharField(max_length=4,blank=False)
+    cvv = models.CharField(max_length=4,blank=True)
     brand = models.CharField(max_length=30,blank=False)
 
     def __str__(self):
