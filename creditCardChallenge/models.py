e0c0ce17d4d682e072f77ea4415c448123b519dd
JKobayashi98 <james.m.kobayashi@gmail.com>
2021-01-31 15:04:12 -0300

    changing the types in the model and adding brand

--- creditCardChallenge/models.py
+++ creditCardChallenge/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 class CreditCard(models.Model):
-    exp_date = models.DateField()
-    holder = models.CharField(max_length=30)
-    number = models.IntegerField()
-    cvv = models.IntegerField()
+    exp_date = models.Charfield(max_length=10,blank=False)
+    holder = models.CharField(max_length=30,blank=False)
+    number = models.CharField(max_length=16,blank=False)
+    cvv = models.CharField(max_length=4,blank=False)
+    brand = models.CharField(max_length=30,blank=False)
 
     def __str__(self):
         return self.holder
