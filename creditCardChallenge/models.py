b6791e946e5506cc07ae3c6b64f01dda8ffc1f29
JKobayashi98 <james.m.kobayashi@gmail.com>
2021-01-31 16:02:29 -0300

    removing useless function from models

--- creditCardChallenge/models.py
+++ creditCardChallenge/models.py
@@ -6,6 +6,3 @@ class CreditCard(models.Model):
     number = models.CharField(max_length=16,blank=False)
     cvv = models.CharField(max_length=4,blank=True)
     brand = models.CharField(max_length=30,blank=False)
-
-    def __str__(self):
-        return self.holder
