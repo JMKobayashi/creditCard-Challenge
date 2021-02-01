2df22777c3f434d12c7f7f321880f8b081c621fd
JKobayashi98 <james.m.kobayashi@gmail.com>
2021-02-01 18:12:45 -0300

    Serializer to get the credit card details

--- creditCardChallenge/serializer.py
+++ creditCardChallenge/serializer.py
@@ -10,3 +10,8 @@ class CreditCardSerializer(serializers.ModelSerializer):
         representation = super(CreditCardSerializer, self).viewRepresentation(instance)
 
         return representation
+
+class CreditCardDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CreditCard
+        exclude = ['id']
