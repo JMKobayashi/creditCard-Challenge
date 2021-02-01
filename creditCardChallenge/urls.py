ebb4e76b3dff9b2b5190371e0629d5eaef65e769
JKobayashi98 <james.m.kobayashi@gmail.com>
2021-02-01 18:19:37 -0300

    Defining url for view CreditCardDetail

--- creditCardChallenge/urls.py
+++ creditCardChallenge/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'listCreditCard', views.creditCardsView.as_view(), name='creditCard')
+    url(r'^list-credit-card/', views.creditCardsView.as_view(), name='credit-cards-list')
+    url(r'^credit-card-detail/<int:pk>',views.creditCardDetail.as_view(), name='credit-card-detail')
 ]
