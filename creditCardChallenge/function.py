55bb534f5c1295862964a170b49ad9be7fdc6a5c
JKobayashi98 <james.m.kobayashi@gmail.com>
2021-02-03 22:52:53 -0300

    function to encrypt the number from the card

--- creditCardChallenge/function.py
+++ creditCardChallenge/function.py
@@ -1,4 +1,14 @@
-
+import cryptography.fernet import Fernet
 
 class creditCardFunction:
     
+    def __init__(self):
+        self.key = "This_is_not_a_secret_key"
+
+
+    def encrypt(value):
+        encoded_value = value.encode()
+        fernet = Fernet(self.key)
+        encrypted_value = fernet.encrypt(encoded_value) 
+
+        return encrypted_value.decode("utf-8")
