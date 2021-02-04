47bebc170d46af8e44c83a1a3da6b1b712c7671f
JKobayashi98 <james.m.kobayashi@gmail.com>
2021-02-03 23:34:26 -0300

    removing encypt file and using key in the function

--- creditCardChallenge/function.py
+++ creditCardChallenge/function.py
@@ -3,7 +3,7 @@ from cryptography.fernet import Fernet
 class creditCardFunction:
     
     def encrypt(value):
-        key = open("encrypt.key",'rb').read()
+        key = b'8lH6QQcT1uQ-7fM0JOg1qZWtSuKbxWOO11i3ytXKDjA='
         encoded_value = value.encode()
         fernet = Fernet(key)
         encrypted_value = fernet.encrypt(encoded_value) 
