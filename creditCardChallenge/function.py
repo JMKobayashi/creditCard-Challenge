1a33ee294976cb7b514ee01d12f0c4c37b7d4f54
JKobayashi98 <james.m.kobayashi@gmail.com>
2021-02-03 23:41:14 -0300

    decrypt function

--- creditCardChallenge/function.py
+++ creditCardChallenge/function.py
@@ -9,3 +9,12 @@ class creditCardFunction:
         encrypted_value = fernet.encrypt(encoded_value) 
 
         return encrypted_value
+
+    def decrypt(value):
+        key = b'8lH6QQcT1uQ-7fM0JOg1qZWtSuKbxWOO11i3ytXKDjA='
+        fernet = Fernet(key)
+        decrypted_value = fernet.decrypt(value)
+
+        decrypted_value.decode('utf-8')
+
+        return decrypted_value
