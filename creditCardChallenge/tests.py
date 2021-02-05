454400c2dd04eb5068596b75f7a51428f64e337b
JKobayashi98 <james.m.kobayashi@gmail.com>
2021-02-05 08:19:46 -0300

    correcting 'resquests' to 'request'

--- creditCardChallenge/tests.py
+++ creditCardChallenge/tests.py
@@ -14,6 +14,6 @@ def test_token_authentication(username,password,status):
         "password":password
     }
 
-    response = resquests.post('http://localhost:8000/token/',user)
+    response = requests.post('http://localhost:8000/token/',user)
 
     assert response.status_code == status
