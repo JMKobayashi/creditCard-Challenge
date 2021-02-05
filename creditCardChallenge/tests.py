b6871586dd00e3e078a8e176d0e67abe09812a9e
JKobayashi98 <james.m.kobayashi@gmail.com>
2021-02-05 09:04:10 -0300

    Changing field "holder"

--- creditCardChallenge/tests.py
+++ creditCardChallenge/tests.py
@@ -23,13 +23,12 @@ def test_token_authentication(username,password,status):
     'exp_date,holder,number,cvv,status',[
         ("07/21","Test 1","4539578763621486","123",201),#succes
         ("07/21","Test 2","4539578763621486","1234",201),#succes
-        ("07/2021","Test 3","4539578763621486","",201),#succes
-        ("07-21","Test 5","4539578763621486","123",201),#succes
+        ("07-21","Test 3","4539578763621486","123",201),#succes
         ("12/20","Test 4","4539578763621486","123",400),#exp_date is in the past!!
         ("07-21","Te","4539578763621486","123",400),#Holder has less than 3 characters
-        ("07-21","Test 7","4539578763620000","124",400),#Credit Card is not valid
-        ("07-21","Test 8","4539578763621486","14",400),#cvv has less tha 3 digits
-        ("07-21","Test 9","4539578763621486","14681",400)#cvv has more than 4 digits
+        ("07-21","Test 6","4539578763620000","124",400),#Credit Card is not valid
+        ("07-21","Test 7","4539578763621486","14",400),#cvv has less tha 3 digits
+        ("07-21","Test 8","4539578763621486","14681",400)#cvv has more than 4 digits
     ]
 )
 
