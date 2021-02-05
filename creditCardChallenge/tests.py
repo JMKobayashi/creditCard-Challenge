1a66997536a4b8dc9fc2cae68554944a18e653a3
JKobayashi98 <james.m.kobayashi@gmail.com>
2021-02-05 08:36:29 -0300

    parameters to post credit card

--- creditCardChallenge/tests.py
+++ creditCardChallenge/tests.py
@@ -17,3 +17,17 @@ def test_token_authentication(username,password,status):
     response = requests.post('http://localhost:8000/token/',user)
 
     assert response.status_code == status
+
+@pytest.mark.parametrize(
+    'exp_date,holder,number,cvv,status',[
+        ("07/21","Test 1","4539578763621486","123",201),#succes
+        ("07/21","Test 2","4539578763621486","1234",201),#succes
+        ("07/2021","Test 3","4539578763621486","",201),#succes
+        ("12/20","Test 4","4539578763621486","123",400),#exp_date is in the past!!
+        ("07-21","Test 5","4539578763621486","123",400),#exp_date is invalid.
+        ("07-21","Tes","4539578763621486","123",400)#Holder has less than 3 characters
+        ("07-21","Test 7","4539578763620000","124",400)#Credit Card is not valid
+        ("07-21","Test 8","4539578763621486","14",400)#cvv has less tha 3 digits
+        ("07-21","Test 9","4539578763621486","14681",400)#cvv has more than 4 digits
+    ]
+)
