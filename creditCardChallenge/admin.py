f2bfb9e90cb6a5e5b600cc0ad7840cf21764face
JKobayashi98 <james.m.kobayashi@gmail.com>
2021-02-05 08:24:14 -0300

    adding 'brand' to list_display in /admin

--- creditCardChallenge/admin.py
+++ creditCardChallenge/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from creditCardChallenge.models import CreditCardModel
 
 class CreditCards(admin.ModelAdmin):
-    list_display = ('id','exp_date','holder','number','cvv')
+    list_display = ('id','exp_date','holder','number','cvv','brand')
     list_display_links = ('id','number')
     search_fields = ('id','number')
     list_per_page = 10
